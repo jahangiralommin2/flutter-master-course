0d6f5098e67f6bce3756a8a576c91c8863535e18
Mehedi Hasan <mehedi@augnitive.com>
2021-04-04 23:03:31 +0600

    clean code

--- smaple_project/lib/main.dart
+++ smaple_project/lib/main.dart
@@ -24,70 +24,68 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text("Drawer Example", style: TextStyle(color: Color(0xff00FF00)),),
         ),
-        endDrawer: Container(
-          height: 300,
-          child: Drawer(
-            child: Column(
-              children: [
-                Container(
-                  child: Stack(
-                    children: [
-                      Image.asset("assets/background.jpg"),
-                      Positioned(
-                        left: 50,
-                        top: 30,
-                        child: CircleAvatar(
-                          radius: 30,
-                          backgroundImage: AssetImage("assets/profile.jpg"),                  
-                        ),
+        //drawer: Drawer(),
+        endDrawer: Drawer(
+          child: Column(
+            children: [
+              Container(
+                child: Stack(
+                  children: [
+                    Image.asset("assets/background.jpg"),
+                    Positioned(
+                      left: 50,
+                      top: 30,
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: AssetImage("assets/profile.jpg"),                  
                       ),
-                      Positioned(
-                        left: 130,
-                        top: 50,
-                        child: Text("Mehedi Hasan", style: TextStyle(color: Colors.white, fontSize: 20),),
-                      ),
-                      Positioned(
-                        left: 130,
-                        top: 80,
-                        child: Text("Flutter Developer", style: TextStyle(color: Colors.white, fontSize: 20)),
-                      ),
-                    ],
-                  ) ,
+                    ),
+                    Positioned(
+                      left: 130,
+                      top: 50,
+                      child: Text("Mehedi Hasan", style: TextStyle(color: Colors.white, fontSize: 20),),
+                    ),
+                    Positioned(
+                      left: 130,
+                      top: 80,
+                      child: Text("Flutter Developer", style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ),
+                  ],
                 ) ,
+              ) ,
 
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text("Profile"),
-                  //subtitle: Text("asfnalfma"),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Profile"),
+                //subtitle: Text("asfnalfma"),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ),
+              Divider(
+                thickness: 2,
+                height: 0,
+                color: Colors.grey,
+              ),
+               ListTile(
+                tileColor: Colors.green,
+                leading: Icon(Icons.notifications),
+                title: Text("Notification"),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ) ,
                 Divider(
-                  thickness: 2,
-                  height: 0,
-                  color: Colors.grey,
-                ),
-                 ListTile(
-                  tileColor: Colors.green,
-                  leading: Icon(Icons.notifications),
-                  title: Text("Notification"),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ) ,
-                  Divider(
-                  thickness: 2,
-                  height: 0,
-                  color: Colors.grey,
-                ),
-                 ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text("Setting"),
-                  trailing: Icon(Icons.arrow_forward_ios),
-                ) ,
-                  Divider(
-                  thickness: 2,
-                  color: Colors.grey,
-                ),    
-              ],
-            ),
+                thickness: 2,
+                height: 0,
+                color: Colors.grey,
+              ),
+               ListTile(
+                leading: Icon(Icons.settings),
+                title: Text("Setting"),
+                trailing: Icon(Icons.arrow_forward_ios),
+              ) ,
+                Divider(
+                thickness: 2,
+                color: Colors.grey,
+              ),    
+            ],
           ),
         ),
 
